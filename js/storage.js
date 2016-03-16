51628d15067c6bd325c2bf135b86924db094b1bd
xinyin92 <cafebabe1112@gmail.com>
2016-03-16 23:35:36 +0800

    期初值和入库管理

---
+++ js/storage.js
@@ -0,0 +1,146 @@
+// storage.js
+$(function (){
+
+	/**
+	 * 日期选择插件使用格式统一设置
+	 * @type {Object}
+	 */
+	var dateFomat={
+		format: "yyyy-mm-dd",
+	    minView: 2,
+	    language:"zh-CN",
+        autoclose: true,
+        todayBtn: true
+	};
+
+
+	/**
+	 * 期初值录入表添加一行
+	 * @param {String} tid [期初值录入表ID]
+	 */
+	function addTr(tid) {
+		var $tbody = $("#"+tid+" tbody");
+		var tr = $tbody.find("tr").last().html();
+		$tbody.append("<tr>"+tr+"</tr>");
+	}
+
+	/**
+	 * 期初值录入表删除一行
+	 */
+	function removeTr(){
+		var $tr = $(this).parent().parent();
+		var lastTrIdx = $("#iniProTabel>tbody>tr:last").index();
+		if(lastTrIdx === 0){
+			 $("div.ini-prod>table").addClass("hide");
+			 $("#addConfirmBtn").addClass("hide");
+		}else{
+			$tr.remove();
+		}		
+	}
+
+	
+	/**
+	 * 在表格中编辑数据
+	 */
+	function editData(){
+		var $nextAll=$(this).nextAll();
+		$(this).addClass("hide");
+		$nextAll.removeClass("hide");
+		$("a.confirmTd").bind("click", confirmData);
+	}
+
+	
+	/**
+	 * 确认表格中编辑的数据
+	 */
+	function confirmData(){
+		var str;
+		var $prev=$(this).prev();
+		var $editable=$(this).siblings(".editable");
+
+		if($prev[0].nodeName==="SELECT"){
+			str=$(this).prev().find("option:selected").text();
+		}else{
+			if ($prev.val()!=="") {
+				str=$prev.val();
+			}else{
+				str="点击编辑";
+			}			
+		}
+		$editable.text(str).removeClass("hide")
+				.siblings().addClass("hide");
+	}
+
+	/**
+	 * 表格中的checkbox全部改为全选状态
+	 * @param  {String} tid 表格ID
+	 */
+	// function checkAll(tid){
+	// 	var $allCheckbox=$(tid+" tbody tr td").find("input[type=checkbox]");
+	// 	if($(this).prop("checked")==="true"){
+	// 		$allCheckbox.prop("checked", true);
+	// 	}else{			
+	// 		$allCheckbox.prop("checked", false);
+	// 	}		
+	// }
+
+	/////////////
+	// 期初值录入 //
+	/////////////
+
+	
+	//期初值录入日期设置
+	$("#initialDate").datetimepicker(dateFomat);
+	$("#initialDate").on("click",function(){
+			$(this).datetimepicker("setEndDate", new Date());  //期初值录入日期不能晚于当前日期
+	});
+	
+	//添加一行期初值录入行
+	$("#newTrBtn").on("click", function(){
+		var idx = $("div.ini-prod>table.hide").index();
+		if (idx !== -1) {
+			$("#iniProTabel").removeClass("hide");
+			$("#addConfirmBtn").removeClass("hide");
+		}else{
+			addTr("iniProTabel");
+			var $a=$("#iniProTabel>tbody>tr:last>td>a.removeTr");
+			$a.bind("click", removeTr);
+			$("span.editable").bind("click",editData);
+		} 
+	});
+
+	//删除一行
+	$("a.removeTr").on("click", removeTr);
+
+	//确认录入期初值
+	$("#addConfirmBtn").on("click",function () {
+		$("div.ini-prod>table").addClass("hide");
+		$("#addConfirmBtn").addClass("hide");
+	});
+
+
+	///////////
+	// 入库管理 //
+	///////////
+	
+	//查询日期范围设置
+	$("#startDate").datetimepicker(dateFomat);
+	$("#endDate").datetimepicker(dateFomat);
+
+	$("#startDate").on("click",function(){
+			$(this).datetimepicker("setEndDate", $("#endDate").val());
+	});
+
+	$("#endDate").on("click",function(){
+			$(this).datetimepicker("setStartDate", $("#startDate").val());
+	});
+
+	//全选
+	// $("#checkAll").on("click", function(){
+	// 	checkAll("#inStoTable")
+	// });
+	
+
+
+
+});
